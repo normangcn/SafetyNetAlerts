41b637ac184d2222e3341f9fca10b70fe22d28b0
Gareth NORMAN <norman.gareth@protonmail.com>
2024-05-12 15:39:01 +0800

    CommunityEmail get loop 1
    Setting up the loop. TODO finish annotations and declarations.

--- src/main/java/com/oc/safetynetalerts/controller/CommunityEmailController.java
+++ src/main/java/com/oc/safetynetalerts/controller/CommunityEmailController.java
@@ -3,13 +3,22 @@
  */
 package com.oc.safetynetalerts.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author gareth
  *
  */
+@RequestMapping("/communityEmail")
+@RestController
+@Slf4j
 public class CommunityEmailController {
 	/*
 	 * http://localhost:8080/communityEmail?city=<city> Cette url doit retourner les
 	 * adresses mail de tous les habitants de la ville.
 	 */
+	
 }
