9293006266ff8ccaec137cbcf13ccdb53b5c8df5
Gareth NORMAN <norman.gareth@protonmail.com>
2024-05-12 15:34:55 +0800

    FloodStation get loop 5
    Loop finished

--- src/main/java/com/oc/safetynetalerts/controller/FloodController.java
+++ src/main/java/com/oc/safetynetalerts/controller/FloodController.java
@@ -3,6 +3,7 @@
  */
 package com.oc.safetynetalerts.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oc.safetynetalerts.model.FireStation;
 import com.oc.safetynetalerts.model.PeopleAndTheirMedicalRecord;
+import com.oc.safetynetalerts.utils.DateUtils;
 
 import static com.oc.safetynetalerts.repository.GlobalRepo.fireStation;
 import static com.oc.safetynetalerts.repository.GlobalRepo.peopleAndtheirMedicalRecords;
 
 import DTOs.FloodStationsStationsOutDTO;
+import DTOs.Residents;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -40,29 +43,41 @@ public class FloodController {
 	
 	@GetMapping("/stations")
 	@ResponseBody
-	public List<FloodStationsStationsOutDTO> floodStation(@RequestParam List<String> station){
+	public List<FloodStationsStationsOutDTO> floodStation(@RequestParam List<String> stations){
 		List<FloodStationsStationsOutDTO> responseDTO = new ArrayList<FloodStationsStationsOutDTO>();
 		List<FireStation> allFireStations = fireStation;
 		List<PeopleAndTheirMedicalRecord> allPeopleAndTheirMedicalrecords = peopleAndtheirMedicalRecords;
 		List<FireStation> fireStationAddresses = new ArrayList<>();
 		for(FireStation fireStationElement : allFireStations) {
-			if(fireStationElement.getStation().equals(String.valueOf(station))) {
+			for(String stationElement:stations) {				
+			if(fireStationElement.getStation().equals(String.valueOf(stationElement))) {
 				FireStation fireStationAddress = new FireStation();
 				fireStationAddress.setAddress(fireStationElement.getAddress());
 				fireStationAddresses.add(fireStationAddress);
 			}
 		}
-		
+		}
 			for(FireStation fireStationElementSecondLoop : fireStationAddresses) {
 				FloodStationsStationsOutDTO floodStationsStation = new FloodStationsStationsOutDTO();
+				List<Residents> residents = new ArrayList<>();
 				floodStationsStation.setAddress(fireStationElementSecondLoop.getAddress());
-				for(PeopleAndTheirMedicalRecord peopleAndTheirMedicalRecordElement : allPeopleAndTheirMedicalrecords)
-				if(peopleAndTheirMedicalRecordElement.getAddress().equals(String.valueOf(fireStationElementSecondLoop.getAddress()))) {
+				for(PeopleAndTheirMedicalRecord peopleAndTheirMedicalRecordElement : allPeopleAndTheirMedicalrecords) {
 					
-				
-				}
+				if(peopleAndTheirMedicalRecordElement.getAddress().equals(String.valueOf(fireStationElementSecondLoop.getAddress()))) {	
+					LocalDate birthDate;
+					Residents resident = new Residents();
+					resident.setFullName(peopleAndTheirMedicalRecordElement.getFullName());
+					resident.setPhoneNumber(peopleAndTheirMedicalRecordElement.getPhone());
+					birthDate = DateUtils.stringToLocalDateFormatter(peopleAndTheirMedicalRecordElement.getBirthdate());				
+					resident.setAge(DateUtils.calculateAge(birthDate));
+					resident.setMedications(peopleAndTheirMedicalRecordElement.getMedications());
+					resident.setAllergies(peopleAndTheirMedicalRecordElement.getAllergies());
+					residents.add(resident);
+				}	
+				floodStationsStation.setResidents(residents);
 			}
-		
+				responseDTO.add(floodStationsStation);
+			}
 		
 		return responseDTO;
 	}
