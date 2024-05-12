83c2f96872dee5bfdb64122c2b3547838dffaa70
Gareth NORMAN <norman.gareth@protonmail.com>
2024-05-12 16:03:54 +0800

    CommunityEmail get loop 2
    Loop done

--- src/main/java/DTOs/CommunityEmailCityOutDTO.java
+++ src/main/java/DTOs/CommunityEmailCityOutDTO.java
@@ -13,29 +13,14 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommunityEmailCityOutDTO {
-	private String personCity;
-	private String personZipCode;
-	private String personEmail;
-	
-	public String getPersonCity() {
-		return personCity;
+	private String email;
+
+	public String getEmail() {
+		return email;
 	}
-	public void setPersonCity(String personCity) {
-		this.personCity = personCity;
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public String getPersonZipCode() {
-		return personZipCode;
-	}
-	public void setPersonZipCode(String personZipCode) {
-		this.personZipCode = personZipCode;
-	}
-	public String getPersonEmail() {
-		return personEmail;
-	}
-	public void setPersonEmail(String personEmail) {
-		this.personEmail = personEmail;
-	}
-	
-	
 
 }
