d9422ec53968523f699430d1d963220cde3779ff
Gareth NORMAN <norman.gareth@protonmail.com>
2024-06-20 14:55:30 +0800

    Quick fix of contextload for tests
    title

--- src/test/java/test/safetynetalerts/SafetyNetAlertsApplicationTests.java
+++ src/test/java/test/safetynetalerts/SafetyNetAlertsApplicationTests.java
@@ -3,7 +3,9 @@ package test.safetynetalerts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.oc.safetynetalerts.SafetyNetAlertsApplication;
+
+@SpringBootTest(classes= SafetyNetAlertsApplication.class )
 class SafetyNetAlertsApplicationTests {
 
 	@Test
